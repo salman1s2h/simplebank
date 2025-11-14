aeb0444edc79ab5a5061ac6c3ba30eb90f0e39be
salman1s2h <salman1s2h@gmail.com>
2025-11-14 21:53:44 +0530

    added .env file

--- main.go
+++ main.go
@@ -2,30 +2,42 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq" // Import Postgres driver
 	"github.com/salman1s2h/simplebank/api"
 	db "github.com/salman1s2h/simplebank/db/sqlc"
+	"github.com/salman1s2h/simplebank/util"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://admin:admin@localhost:5433/go_db?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://admin:admin@localhost:5433/go_db?sslmode=disable"
+// 	serverAddress = "0.0.0.0:8080"
+// )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	env_obj := util.NewEnv()
+
+	fmt.Printf("Environment: %+v\n", env_obj.AppEnv)
+
+	dbSource := fmt.Sprintf(
+		"postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+		env_obj.DBUser, env_obj.DBPass, env_obj.DBHost, env_obj.DBPort, env_obj.DBName,
+	)
+	conn, err := sql.Open(env_obj.DB_DRIVER, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 	store := db.NewStore(conn)
 
 	server := api.NewServer(store)
-	log.Printf("Starting server at %s", serverAddress)
+	serverAddr := fmt.Sprintf("%s:%s", env_obj.ServerAddress, env_obj.APP_PORT)
 
-	err = server.Start(serverAddress)
+	log.Printf("Starting server at %s", serverAddr)
+
+	err = server.Start(serverAddr)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
---
+++ util/config.go
@@ -0,0 +1,48 @@
+package util
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Env struct {
+	AppEnv                 string `mapstructure:"APP_ENV"`
+	ServerAddress          string `mapstructure:"SERVER_ADDRESS"`
+	ContextTimeout         int    `mapstructure:"CONTEXT_TIMEOUT"`
+	DBHost                 string `mapstructure:"DB_HOST"`
+	DBPort                 string `mapstructure:"DB_PORT"`
+	DBUser                 string `mapstructure:"DB_USER"`
+	DBPass                 string `mapstructure:"DB_PASS"`
+	DBName                 string `mapstructure:"DB_NAME"`
+	AccessTokenExpiryHour  int    `mapstructure:"ACCESS_TOKEN_EXPIRY_HOUR"`
+	RefreshTokenExpiryHour int    `mapstructure:"REFRESH_TOKEN_EXPIRY_HOUR"`
+	AccessTokenSecret      string `mapstructure:"ACCESS_TOKEN_SECRET"`
+	RefreshTokenSecret     string `mapstructure:"REFRESH_TOKEN_SECRET"`
+	DB_DRIVER              string `mapstructure:"DB_DRIVER"`
+	SERVER_ADDRESS         string `mapstructure:"SERVER_ADDRESS"`
+	APP_PORT               string `mapstructure:"APP_PORT"`
+}
+
+func NewEnv() *Env {
+	env := Env{}
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+	viper.SetConfigName("app.env")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("Can't find the file .env : ", err)
+	}
+
+	err = viper.Unmarshal(&env)
+	if err != nil {
+		log.Fatal("Environment can't be loaded: ", err)
+	}
+
+	if env.AppEnv == "development" {
+		log.Println("The App is running in development env")
+	}
+
+	return &env
+}
